199c49874cf40c3bcbc0c8c7a99be4515c2c4d89
Aidan Smith <aidan.smith001@albright.edu>
2025-09-05 11:18:19 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,9 @@
+# There are all kinds of different books for Python that come in a variety of languages. Examples include programming, game design and computer science.
+
+# Several jobs for Python seem to be directed towards developers and engineers. These people deal with problems revolving around Ai, Data and software just to name a few.
+
+# The education for Python comes in a variety of languages including english, Japanese, korean, russian and even persian.
+
+# Python has all kinds of books and educational recourses for all kinds of levels from noobie to expert.
+
+# It turns out that Python is so succesful, that it has its own convention dubbed,PyCon.
