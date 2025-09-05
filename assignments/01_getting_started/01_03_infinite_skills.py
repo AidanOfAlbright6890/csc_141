eea9b920a01e648b5dd3e0f08973f0645c54212c
Aidan Smith <aidan.smith001@albright.edu>
2025-09-05 11:56:09 -0400

    01_03_infinite_skills.py

---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,20 @@
+'''If I had infinite programming skills, one thing I would program are ideas for video games and animations.'''
+
+'''I would also program my art portfolio including projects drawn or modeled on Maya or Unreal Engine.'''
+
+'''One other thing that I would program are the talents I use for my projects including modeling, drawing and'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
