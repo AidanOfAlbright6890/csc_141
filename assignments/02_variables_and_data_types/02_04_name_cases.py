3364dd8353c54f267c1788bfef657decf78c45e2
Aidan Smith <aidan.smith001@albright.edu>
2025-09-08 17:45:51 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_04_name_cases.py
@@ -0,0 +1,7 @@
+name = "Andrew Robinson"
+print(name.upper())
+print(name.lower())
+first_name = "andrew"
+last_name = "robinson"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
