d6bfc86bbe8b8f9de2c646f094a080f50df28223
Aidan Smith <aidan.smith001@albright.edu>
2025-09-08 17:51:13 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_07_stripping_names.py
@@ -0,0 +1,10 @@
+print("\nMaya")
+print("\tMaya")
+print("Languages:\nMaya\nC\nJavaScript")
+
+
+print("Languages:\n\tMaya\n\tC\n\tJavaScript")
+favorite_language = ' Maya '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
