42b1edd5f6dded302fe1474d879525883ed4b945
Aidan Smith <aidan.smith001@albright.edu>
2025-09-08 18:27:46 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_02_simple_messages.py
@@ -0,0 +1,63 @@
+message = "who's hungry for python power?"
+print(message)
+message = "who's hungry for python power?"
+print(message)
+'''The first message prints like normal, but when I move the variable to another message, both messages are printed'''
+
+"This is a string"
+'This is also a string'
+'I told my friend, "Python is my favorite language!"'
+"The language 'Python' is named after Monty Python, not the snake."
+"One of Python's strengths is its diverse and supportive community."
+name = "ada lovelace"
+print(name.title())
+'''I think what is happening with the 2 lines above is that the name being printed is being renamed as if it where a title to a book or film.'''
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Python")
+
+print("\tPython")
+
+
+print("Languages:\nPython\nC\nJavaScript")
+
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+
+
+favorite_language = 'python '
+favorite_language
+favorite_language.rstrip()
+favorite_language
+
+favorite_language = 'python '
+favorite_language = favorite_language.rstrip()
+favorite_language
+
+favorite_language = ' python '
+favorite_language.rstrip()
+favorite_language.lstrip()
+favorite_language.strip()
+
+nostarch_url = 'https://nostarch.com'
+nostarch_url.removeprefix('https://')
+
+message = "One of Python's strengths is its diverse community."
+print(message)
