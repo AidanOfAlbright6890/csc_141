dcf40d549fd7b26ad8c3761967f00dcc8d118d5c
Aidan Smith <aidan.smith001@albright.edu>
2025-09-08 18:24:01 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_11_adding_comments.py
@@ -0,0 +1,18 @@
+message = ("program 1: Python")
+print(message)
+# Praise Python power
+print("Python is one of the most powerful programs I have ever used so far.")
+print("Being the power house Python is, this beast plays a crucial rule in the creation of a huge variety of digital media and entertainment.")
+
+
+message = ("program 2:Visual Studio Code")
+print(message)
+# praise Visual Studio Code
+print("Visual Studio Code is a very useful program to use for Python.")
+print("Visual Studio Code acts as a tool for Python where the programmer can type in their codes.")
+
+print("The Zen of Python")
+
+import this 
+"The Zen of Python, by Tim Peters Beautiful is better than ugly." 
+
