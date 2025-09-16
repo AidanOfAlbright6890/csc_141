af6687a2863ebe880e125645eda8ed86d2d66123
Aidan Smith <aidan.smith001@albright.edu>
2025-09-16 09:05:54 -0400

    Delete assignments/Introducing Lists directory

--- assignments/Introducing Lists/03_09_dinner_guests.py
+++
@@ -1,4 +0,0 @@
-"Dinner Guests"
-Guests_for_Dinner = ['David Kaul', 'John Pankratz']
-print(len(Guests_for_Dinner))
-
--- assignments/Introducing Lists/03_11_intentional_error.py
+++
@@ -1,4 +0,0 @@
-"Intentional error"
-Guests_for_Dinner = ['David Kaul', 'John Pankratz', 'Mark Baldridge']
-print(Guests_for_Dinner[2])
-"In this program, I used an index with the number 3 to represent Mark Baldridge. However, because Indexes start with 0, the proper number I should type is 2."
