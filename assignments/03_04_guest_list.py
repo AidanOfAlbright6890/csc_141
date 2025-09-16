0c49a4910eb646e144341e7e7e7e0eb35def0749
Aidan Smith <aidan.smith001@albright.edu>
2025-09-16 08:48:02 -0400

    Delete assignments/03_04_guest_list.py

--- assignments/03_04_guest_list.py
+++
@@ -1,9 +0,0 @@
-print("Guest List")
-Guests_for_Dinner = ['David Kaul', 'John Pankratz', 'Scott Deloach']
-print(Guests_for_Dinner[0])
-print(Guests_for_Dinner[1])
-print(Guests_for_Dinner[2])
-print("David Kaul, I invite you to dinner.")
-print("John Pankratz, I invite you to dinner.")
-print("Scott Deloach, I invite you to dinner.")
-
